59d3a6db693d40e3a81dce4a6f90bfd83652133f
Hegewald1 <eaamheg@students.eaaa.dk>
2021-12-14 15:56:35 +0100

    exercise 3 + 4 pretty much done might have to change moving from coord1 to coord2 in exercise4

--- Exercise3/ok_lesgo.py
+++ Exercise3/ok_lesgo.py
@@ -1,11 +1,11 @@
 # -*- coding: utf-8 -*-
 """
-Created on Wed Nov 10 11:23:16 2021
+Created on Wed Nov 16 17:43:34 2021
 
 @author: sila
 """
 
-# CODE FROM NOTEBOOK, Exercise 4, ML WEEK 9
+# CODE FROM NOTEBOOK, Exercise 6, ML WEEK 9
 
 # Deep Learning Neural Network for the CIFAR-10 dataset
 # Nov. 16th 2021
@@ -14,8 +14,8 @@ Created on Wed Nov 10 11:23:16 2021
 import numpy as np
 import matplotlib.pyplot as plt
 
-from keras.layers import Input, Flatten, Dense, Conv2D
-from keras.models import Model
+from keras.layers import Input, Flatten, Dense, Conv2D, BatchNormalization, LeakyReLU, Dropout, Activation, MaxPooling2D
+from keras.models import Model, Sequential
 from tensorflow.keras.optimizers import Adam
 from tensorflow.keras.utils import to_categorical
 
@@ -39,31 +39,31 @@ x_train[5400, 17, 9, 1]
 
 x_test.shape
 
-# Describe the model that we will use here 
+model = Sequential()
+model.add(Conv2D(32, (3, 3), activation='relu', kernel_initializer='he_uniform', padding='same', input_shape=(32, 32, 3)))
+model.add(Conv2D(64, (3, 3), activation='relu', kernel_initializer='he_uniform', padding='same',))
+model.add(BatchNormalization())
+model.add(LeakyReLU())
+model.add(Dropout(rate=0.5))
 
-input_layer = Input((32,32,3))
+model.add(Flatten())
 
-x = Flatten()(input_layer)
-
-x = Dense(200, activation = 'relu')(x)
-x = Dense(150, activation = 'relu')(x)
-
-output_layer = Dense(NUM_CLASSES, activation = 'softmax')(x)
-
-model = Model(input_layer, output_layer)
+model.add(Dense(128, activation='relu', kernel_initializer='he_uniform'))
+model.add(Dense(10, activation='softmax'))
 
 model.summary()
 
-opt = Adam(learning_rate=0.0005)
+opt = Adam(learning_rate=0.001)
 model.compile(loss='categorical_crossentropy', optimizer=opt, metrics=['accuracy'])
 
 model.fit(x_train
           , y_train
           , batch_size=32
           , epochs=10
-          , shuffle=True)
+          , shuffle=True
+          , validation_data = (x_test, y_test))
 
-model.evaluate(x_test, y_test)
+model.evaluate(x_test, y_test, batch_size=1000)
 
 CLASSES = np.array(['airplane', 'automobile', 'bird', 'cat', 'deer', 'dog', 'frog', 'horse', 'ship', 'truck'])
 
@@ -83,6 +83,6 @@ for i, idx in enumerate(indices):
     img = x_test[idx]
     ax = fig.add_subplot(1, n_to_show, i+1)
     ax.axis('off')
-    ax.text(0.5, -0.35, 'pred = ' + str(preds_single[idx]), fontsize=10, ha='center', transform=ax.transAxes) 
+    ax.text(0.5, -0.35, 'pred = ' + str(preds_single[idx]), fontsize=10, ha='center', transform=ax.transAxes)
     ax.text(0.5, -0.7, 'act = ' + str(actual_single[idx]), fontsize=10, ha='center', transform=ax.transAxes)
     ax.imshow(img)
